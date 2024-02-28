ee02ee6e353632fd1c57cb5f236957ca0c753968
Jeremiah <jeremherring@gmail.com>
2024-02-28 12:38:56 -0800

    make keywords.py

---
+++ Lexer FSM/Keywords.py
@@ -0,0 +1,17 @@
+keywords = {
+    'integer', 'if', 'else', 'endif', 'while'
+    'return', 'scan', 'print', 'boolean', 'real',
+    'function', 'true', 'false'
+    }
+
+def validate_keyword(keyword):
+    if keyword in keywords:
+        return True
+    return False
+    
+# testing
+keyword = "scan"
+if validate_keyword(keyword):
+    print(f"{keyword} is a keyword")
+else:
+    print(f"{keyword} is a not keyword")
