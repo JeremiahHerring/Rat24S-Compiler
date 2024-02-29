40410df950c0c40daea404a6b00cbb6bcd9b0ebb
Jeremiah <jeremherring@gmail.com>
2024-02-29 12:34:08 -0800

    make operator and separator list

--- Lexer FSM/Operator.py
+++ Lexer FSM/Operator.py
@@ -1,1 +1,13 @@
-operators = {}
+operators = {"==", "!=", ">", "<", "<=", "=>", "*", "/", "+", "-"}
+
+def validate_operator(operator):
+    if operator in operators:
+        return True
+    return False
+    
+# testing
+operator = "!="
+if validate_operator(operator):
+    print(f"{operator} is a operator")
+else:
+    print(f"{operator} is a not operator")
---
+++ Lexer FSM/Separator.py
@@ -0,0 +1,13 @@
+separators = {';', ',', '(', ')', '{', '}', '[', ']'}
+
+def validate_separator(separator):
+    if separator in separators:
+        return True
+    return False
+    
+# testing
+separator = ","
+if validate_separator(separator):
+    print(f"{separator} is a separator")
+else:
+    print(f"{separator} is a not separator")
