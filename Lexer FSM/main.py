b9c32c51124a1def055386a91b90bf125eb99f93
Noah Beidelman <nbeidelman9@gmail.com>
2024-02-29 20:39:08 -0800

    concept for the main file

--- Lexer FSM/main.py
+++ Lexer FSM/main.py
@@ -6,39 +6,74 @@ from Separator import validate_separator, separators
 from Keywords import validate_keyword, keywords
 
 def main():
+    with open('./input.txt', 'r') as file:
+        # content contains the whole input file, you can access each character of the inputstring -> content[i]
+        content = file.read()
+
+    # given a string, will print the token type and lexeme
+    lexer(content)
+
     # Make instances of each finite state machine
     identifier_fsm = IdentifierFSM()
     integer_fsm = IntegerFSM()
     real_fsm = RealLexer()
 
-    endOfFile = False
-    # while not endOfFile:
-    #     lexer(string)
+    
+    def lexer(char):
+        lexeme = ""
+        inputCharTerminatesToken = False
+        for char in content:
+            
+            # if we are in one of the finite state machines and we get to a separator, then we should return a value we can identify from the validate_x functions
+            # (not just reject or false) so we can tell if inputCharTerminatesToken
+            if inputCharTerminatesToken and state == accepting:
+                token = determineToken()
+                print(f"Token : {token}, Lexeme: {lexeme}") # can print here or maybe append to list [(token, lexeme), etc.] to print after?
+                lexeme = ""
+                inputCharTerminatesToken = False
+            else:
+                validate_operator(char)
+                validate_separator(char)
+                validate_keyword(char)
+                identifierState = identifier_fsm.validate_identifier(char, prevState)
+                realState = real_fsm.validate_real(char, prevState)
+                integerState = integer_fsm.validate_integer(char, prevState)
 
-    def lexer(string):
-        # Pass string to each FSM
+                # maybe for this we return 2 values to help us determine if token was terminated (token terminated when we are at accepting state in prevState and then we go to nonaccepting?)
+                integerState, thingToHelpDetermineTerminatedToken = integer_fsm.validate_integer(char, prevState)
 
-        if identifier_fsm.validate_identifier(string):
-            return (f"Token : Identifier, Lexeme: {string}")
-        elif integer_fsm.validate_integer(string):
-            return (f"Token : Integer, Lexeme: {string}")
-        elif real_fsm.validate_real(string):
-            return (f"Token : Real, Lexeme: {string}")
-        elif validate_keyword(string):
-            return (f"Token : Keyword, Lexeme: {string}")
-        elif validate_operator(string):
-            return (f"Token : Operator, Lexeme: {string}")
-        elif validate_separator(string):
-            return (f"Token : Separator, Lexeme: {string}")
-        else:
-            return "Invalid Token Type"
-
-    # Going to have to implement some logic that lets the lexer know how to separate operators and separators when there is no white space between them and other tokens
+                # logic hear using the returned states that will let us know if the token was terminated.
+                # ^ update inputuCharTerminatesToken
+                if thingToHelpDetermineTerminatedToken:
+                    inputCharTerminatesToken = True
         
-    # Test the Lexer
-    test_case = ["while", "x", "!=", "9", "123.13", "["]
-    for string in test_case:
-        print(lexer(string))
+
+
+        
 
 if __name__ == "__main__":
     main()
+
+
+# have a lexer for each type that iterate over the file together. have them each clasify the token as its own type or not its type -- add to a list for each type.
+# then print out at the end in the order that the chars come in
+def identifierLexer(char):
+        lexeme = ""
+        inputCharTerminatesToken = False
+        for char in content:
+            
+            # if we are in one of the finite state machines and we get to a separator, then we should return a value we can identify from the validate_x functions
+            # (not just reject or false) so we can tell if inputCharTerminatesToken
+            if inputCharTerminatesToken and state == accepting: # maybe state will just be true or false so we don't have to do == accepting
+                token = determineToken()
+                print(f"Token : {token}, Lexeme: {lexeme}") # can print here or maybe append to list [(token, lexeme), etc.] to print after?
+                lexeme = ""
+                inputCharTerminatesToken = False
+            else:
+                state = identifier_fsm.validate_identifier(char, prevState)
+                
+
+                # logic hear using the returned states that will let us know if the token was terminated.
+                # ^ update inputuCharTerminatesToken
+                if something:
+                    inputCharTerminatesToken = True
