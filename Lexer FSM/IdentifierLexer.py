23d0891a08c7b77c90c53959c6525a0c00c754e1
Jeremiah <jeremherring@gmail.com>
2024-03-01 21:08:34 -0800

    BIG BOY CHANGES

--- Lexer FSM/IdentifierLexer.py
+++ Lexer FSM/IdentifierLexer.py
@@ -1,32 +1,33 @@
-from Keywords import keywords
+from Separator import separators
+from Operator import operators
+
 class IdentifierFSM:
     def __init__(self):
-        self.states = {'1', '2', '3', '4', '5', '6'} # Initialize three states
-        self.starting_state = '1' 
-        self.current_state = ''
+        self.states = {'1', '2', '3', '4', '5', '6'}
+        self.starting_state = '1'
+        self.current_state = self.starting_state
         self.accepting_states = {'2', '4', '5', '6'}
         self.transition_table = {
-            ('1', 'letter') : '2',
-            ('1', 'digit') : '3',
-            ('1', '_') : '3',
-            ('2', 'letter') : '4',
-            ('2', 'digit') : '5',
-            ('2', '_') : '6',
-            ('3', 'letter') : '3',
-            ('3', 'digit') : '3',
-            ('3', '_') : '3',
-            ('4', 'letter') : '4',
-            ('4', 'digit') : '5',
-            ('4', '_') : '6',
-            ('5', 'letter') : '4',
-            ('5', 'digit') : '5',
-            ('5', '_') : '6',
-            ('6', 'letter') : '4',
-            ('6', 'digit') : '5',
-            ('6', '_') : '6',
+            ('1', 'letter'): '2',
+            ('1', 'digit'): '3',
+            ('1', '_'): '3',
+            ('2', 'letter'): '4',
+            ('2', 'digit'): '5',
+            ('2', '_'): '6',
+            ('3', 'letter'): '3',
+            ('3', 'digit'): '3',
+            ('3', '_'): '3',
+            ('4', 'letter'): '4',
+            ('4', 'digit'): '5',
+            ('4', '_'): '6',
+            ('5', 'letter'): '4',
+            ('5', 'digit'): '5',
+            ('5', '_'): '6',
+            ('6', 'letter'): '4',
+            ('6', 'digit'): '5',
+            ('6', '_'): '6',
         }
 
-    # Function to process input
     def process_input(self, char):
         if char.isalpha():
             return 'letter'
@@ -37,30 +38,46 @@ class IdentifierFSM:
         else:
             return None
 
-    # Function to validate or invalidate identifier
-    def validate_identifier(self, identifier):
-        # First check if the identifier is a keyword
-        if identifier in keywords:
-            return False
-        self.current_state = self.starting_state # Have each identifier in the start position
-        for char in identifier:
-            input_type = self.process_input(char)
-            if input_type is None:
-                self.current_state = '3'
-                break
-            if (self.current_state, input_type) in self.transition_table:  # Check if char is valid for the state that it is in
-                self.current_state = self.transition_table[(self.current_state, input_type)]   # Transition to state based on the input given
-            else:
-                self.current_state = '3'  # Not valid
+    def process_char(self, char):
+        input_type = self.process_input(char)
 
-        return self.current_state in self.accepting_states  # if current state is accept: return True else return False
+        if input_type is None:
+            self.current_state = '3'
+        elif (self.current_state, input_type) in self.transition_table:
+            self.current_state = self.transition_table[(self.current_state, input_type)]
+        else:
+            self.current_state = '3'
+
+        input_char_terminates_token = (
+            char in separators or char in operators or char.isspace()
+        ) and self.current_state not in self.accepting_states
+
+        return input_char_terminates_token, self.current_state
+
+    def validate_identifier(self, identifier):
+        input_char_terminates_token = False
+        prev_accepting_state = self.starting_state
+
+        for char in identifier:
+            terminates_token, current_state = self.process_char(char)
+
+            if terminates_token:
+                input_char_terminates_token = True
+
+            if current_state in self.accepting_states:
+                prev_accepting_state = current_state
+
+        is_valid = prev_accepting_state in self.accepting_states or (
+            input_char_terminates_token and prev_accepting_state == self.starting_state
+        )
+
+        return input_char_terminates_token, is_valid
 
 if __name__ == "__main__":
     identifier_fsm = IdentifierFSM()
 
-#     identifier = "helloWorld"
-
-#     if identifier_fsm.validate_identifier(identifier):
-#         print(f"{identifier} is a valid identifier")
-#     else:
-#         print(f"{identifier} is not a valid identifier")
+    identifier = "abc#"
+    
+    for char in identifier:
+        input_char_terminates_token, is_valid = identifier_fsm.validate_identifier(char)
+        print(f"Char: {char}, Terminates Token: {input_char_terminates_token}, Is Valid: {is_valid}")
