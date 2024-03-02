60df3a22d64e9556ee9e75deaf802b3c414a38d2
Jeremiah <jeremherring@gmail.com>
2024-03-02 13:50:17 -0800

    make integer and real lexer finite state machines

--- Lexer FSM/RealLexer.py
+++ Lexer FSM/RealLexer.py
@@ -1,10 +1,13 @@
+from Separator import separators
+from Operator import operators
+
 class RealLexer:
     def __init__(self):
         self.states = {'1', '2', '3', '4'}
-        self.alphabet = {'digit', '.'}
+        self.alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'}
         self.start_state = '1'
-        self.accepting_state = '4'
-        self.current_state = ''
+        self.accepting_state = {'4'}
+        self.current_state = self.start_state
         self.transition_table = {
             ('1', 'digit'): '2',
             ('2', 'digit'): '2',
@@ -12,8 +15,8 @@ class RealLexer:
             ('3', 'digit'): '4',
             ('4', 'digit'): '4',
         }
-    
-    # identify char
+
+    # Identify char
     def process_input(self, char):
         if char.isdigit():
             return 'digit'
@@ -22,28 +25,47 @@ class RealLexer:
         else:
             return None
 
+    def process_char(self, char):
+        input_type = self.process_input(char)
+
+        if input_type is None:
+            self.current_state = 'reject'
+        elif (self.current_state, input_type) in self.transition_table:
+            self.current_state = self.transition_table[(self.current_state, input_type)]
+        else:
+            self.current_state = 'reject'
+
+        input_char_terminates_token = (
+            char in separators or char in operators or char.isspace()
+        ) and self.current_state not in self.accepting_state
+
+        return input_char_terminates_token, self.current_state
+
     # Function to validate or invalidate real number
     def validate_real(self, inputStr):
-        self.current_state = self.start_state
-        for char in inputStr:
-            input_type = self.process_input(char)
-            if input_type is None:
-                self.current_state = 'reject'
-                break
-            if (self.current_state, input_type) in self.transition_table:
-                self.current_state = self.transition_table[(self.current_state, input_type)]
-            else:
-                self.current_state = 'reject'
+        input_char_terminates_token = False
+        prev_accepting_state = self.start_state
 
-        return self.current_state in self.accepting_state # if current state is accept: return True else return False
+        for char in inputStr:
+            terminates_token, current_state = self.process_char(char)
+
+            if terminates_token:
+                input_char_terminates_token = True
+
+            if current_state in self.accepting_state:
+                prev_accepting_state = current_state
+
+        is_valid = prev_accepting_state in self.accepting_state or (
+            input_char_terminates_token and prev_accepting_state == self.start_state
+        )
+
+        return is_valid, input_char_terminates_token
 
 if __name__ == "__main__":
     lexerInstance = RealLexer()
-#     tests = ["234.234","234.",".2","2.a", "a.2", ".", "2", "a"]
 
-#     for test in tests:
-#         if lexerInstance.validate_real(test):
-#             print(f"{test} is a valid real number")
-#         else:
-#             print(f"{test} is not a valid real number")
-
+    real = "1.1"
+    
+    for char in real:
+        is_valid, input_char_terminates_token = lexerInstance.validate_real(char)
+        print(f"Char: {char}, Is Valid: {is_valid}, Terminates Token: {input_char_terminates_token}")
