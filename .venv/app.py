104264f093e005670c8403074dcb3dc4d17c02e5
Jeremiah Herring <jeremherring@gmail.com>
2024-03-04 10:33:52 -0800

    making this a web app with flask for some reason HAHAHAHAHA

---
+++ .venv/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return "Index Page"
+
+# @app.route("/hello")
+# def hello():
+#     return "Hello, World"
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
