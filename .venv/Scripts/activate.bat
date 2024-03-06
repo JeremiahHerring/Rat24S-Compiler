b401cfdec6926b8ac3facd13df8be11a05f89bae
Jeremiah <jeremherring@gmail.com>
2024-03-05 21:48:25 -0800

    Making homer

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\zz-jherring\.vscode\Lexical-Analyzer\.venv
+set VIRTUAL_ENV=C:\Users\ThatN\.vscode\Lexical-Analyzer-1\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
