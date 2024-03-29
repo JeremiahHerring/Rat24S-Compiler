894c517097be7eb0abd41e19cdf8c0c72d4ccea4
Jeremiah <jeremherring@gmail.com>
2024-03-28 17:34:54 -0700

    add functions

---
+++ Syntax-Analyzer/Production_Functions.py
@@ -0,0 +1,113 @@
+def rat24s():
+    pass
+
+def optFunctionDefinitions():
+    pass
+
+def functionDefinitions():
+    pass
+
+def functionDefinitions2():
+    pass
+
+def function():
+    pass
+
+def optParameterList():
+    pass
+
+def parameterList():
+    pass
+
+def parameterList2():
+    pass
+
+def parameter():
+    pass
+
+def qualifier():
+    pass
+
+def body():
+    pass
+
+def optDeclarationList():
+    pass
+
+def declarationList():
+    pass
+
+def declarationList2():
+    pass
+
+def declaration():
+    pass
+
+def ids():
+    pass
+
+def ids2():
+    pass
+
+def statementList():
+    pass
+
+def statementList2():
+    pass
+
+def statement():
+    pass
+
+def compound():
+    pass
+
+def assign():
+    pass
+
+def if2():
+    pass
+
+def if2prime():
+    pass
+
+def return2():
+    pass
+
+def return2prime():
+    pass
+
+def print2():
+    pass
+
+def scan2():
+    pass
+
+def while2():
+    pass
+
+def condition():
+    pass
+
+def relop():
+    pass
+
+def expression():
+    pass
+
+def expression2():
+    pass
+
+def term():
+    pass
+
+def term2():
+    pass
+
+def factor():
+    pass
+
+def primary():
+    pass
+
+def empty():
+    pass
