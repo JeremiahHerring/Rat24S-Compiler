317e6133cc6a6be9eb1b26fc975838ede2d7fca3
Noah Beidelman <nbeidelman9@gmail.com>
2024-04-06 18:23:03 -0700

    move primary2

--- Syntax-Analyzer/Production_Functions.py
+++ Syntax-Analyzer/Production_Functions.py
@@ -367,13 +367,3 @@ def empty():
 
 # CALL PROGRAM
 rat24s()
-
-def primary2():
-    print("<Primary'> ::= ( <IDs> ) | ε")
-    if lexerList[i][1] == "(":
-        lexer(True)
-        ids()
-        if lexerList[i][1] == ")":
-            lexer(True)
-    else:
-        pass 
